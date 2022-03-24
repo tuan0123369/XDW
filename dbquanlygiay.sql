295b2c5aad172116b66c404eaaf184c01674b4a4
tuan0123369 <tuan0123369@gmail.com>
2022-03-24 12:05:11 +0700

    upload

---
+++ dbquanlygiay.sql
@@ -0,0 +1,328 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th3 24, 2022 lúc 05:03 AM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 7.4.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `dbquanlygiay`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `blogs`
+--
+
+CREATE TABLE `blogs` (
+  `id_blog` int(11) NOT NULL,
+  `id_categories` int(10) NOT NULL,
+  `name_blog` varchar(100) NOT NULL,
+  `image` varchar(200) NOT NULL,
+  `description` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `blogs`
+--
+
+INSERT INTO `blogs` (`id_blog`, `id_categories`, `name_blog`, `image`, `description`) VALUES
+(1, 3, 'Bài viết 1', 'm-blog-1.jpg', 'dfg'),
+(2, 3, 'Bài viết 2', '', ''),
+(3, 3, 'Bài viết 3', '', ''),
+(4, 3, 'Bài viết 4', '', ''),
+(5, 3, 'Bài viết 5', '', ''),
+(6, 3, 'Bài viết 6', '', ''),
+(7, 3, 'Bài viết 7', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `cart`
+--
+
+CREATE TABLE `cart` (
+  `id_user` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `cart`
+--
+
+INSERT INTO `cart` (`id_user`, `id_product`, `amount`) VALUES
+(1, 1, 1),
+(1, 3, 1),
+(1, 9, 1),
+(1, 6, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `categories`
+--
+
+CREATE TABLE `categories` (
+  `id_categories` int(11) NOT NULL,
+  `category_name` varchar(100) NOT NULL,
+  `status` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `categories`
+--
+
+INSERT INTO `categories` (`id_categories`, `category_name`, `status`) VALUES
+(1, 'Giày nữ', 1),
+(2, 'Giày nam', 1),
+(3, 'Bài viết', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `orders`
+--
+
+CREATE TABLE `orders` (
+  `id_order` int(50) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `update_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `order_details`
+--
+
+CREATE TABLE `order_details` (
+  `id_order` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL,
+  `pricesale` double NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `products`
+--
+
+CREATE TABLE `products` (
+  `id_product` int(11) NOT NULL,
+  `id_categories` int(10) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `image` text NOT NULL,
+  `price` double NOT NULL,
+  `remaining` int(11) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `products`
+--
+
+INSERT INTO `products` (`id_product`, `id_categories`, `product_name`, `description`, `image`, `price`, `remaining`, `status`) VALUES
+(1, 1, 'Giày 1', '', '', 1, 99, 0),
+(2, 1, 'Giày 2', '', '', 2, 99, 0),
+(3, 1, 'Giày 3', '', '', 3, 99, 0),
+(4, 2, 'Giày 4', '', '', 4, 99, 0),
+(5, 2, 'Giày 5', '', '', 5, 0, 0),
+(6, 2, 'Giày mới 1', '', '', 6, 99, 0),
+(7, 2, 'Giày mới 2', '', '', 7, 99, 0),
+(8, 2, 'Giày mới 3', '', '', 8, 99, 0),
+(9, 2, 'Giày mới 4', '', '', 9, 99, 0),
+(10, 2, 'Giày mới 5', '', '', 10, 99, 0),
+(11, 2, 'Giày mới 6', '', '', 11, 99, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(10) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` text NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `avatar` text NOT NULL,
+  `role` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `users`
+--
+
+INSERT INTO `users` (`id_user`, `user_name`, `email`, `password`, `address`, `phone`, `avatar`, `role`) VALUES
+(1, '1', '123@123', '$2y$10$YGCqwuiL2A4wAo5gRQNt5eb/FEexW01BztORrP/kjKtL6K90mvR2e', '123', '111', 'large.png', 0),
+(2, '1', 'admin@admin', '$2y$10$OJ9c83ZGZ7Esy5hNEimSX.TOeKf04Br/3G3GYPzhpOQqVQw7QywW.', '1', '1', 'UNDONE', 0);
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `blogs`
+--
+ALTER TABLE `blogs`
+  ADD PRIMARY KEY (`id_blog`),
+  ADD KEY `blog_categories_fk` (`id_categories`);
+
+--
+-- Chỉ mục cho bảng `cart`
+--
+ALTER TABLE `cart`
+  ADD KEY `cart_user` (`id_user`),
+  ADD KEY `cart_product` (`id_product`);
+
+--
+-- Chỉ mục cho bảng `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id_categories`);
+
+--
+-- Chỉ mục cho bảng `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id_order`),
+  ADD KEY `order_user_fk` (`id_user`);
+
+--
+-- Chỉ mục cho bảng `order_details`
+--
+ALTER TABLE `order_details`
+  ADD KEY `order_detail_order_fk` (`id_order`),
+  ADD KEY `order_detail_product_fk` (`id_product`);
+
+--
+-- Chỉ mục cho bảng `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id_product`),
+  ADD KEY `product_categories_fk` (`id_categories`);
+
+--
+-- Chỉ mục cho bảng `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `blogs`
+--
+ALTER TABLE `blogs`
+  MODIFY `id_blog` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT cho bảng `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id_categories` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT cho bảng `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id_order` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `products`
+--
+ALTER TABLE `products`
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT cho bảng `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `blogs`
+--
+ALTER TABLE `blogs`
+  ADD CONSTRAINT `blog_categories_fk` FOREIGN KEY (`id_categories`) REFERENCES `categories` (`id_categories`);
+
+--
+-- Các ràng buộc cho bảng `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_product` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`),
+  ADD CONSTRAINT `cart_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Các ràng buộc cho bảng `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `order_user_fk` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Các ràng buộc cho bảng `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `order_detail_order_fk` FOREIGN KEY (`id_order`) REFERENCES `orders` (`id_order`),
+  ADD CONSTRAINT `order_detail_product_fk` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`);
+
+--
+-- Các ràng buộc cho bảng `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `product_categories_fk` FOREIGN KEY (`id_categories`) REFERENCES `categories` (`id_categories`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
